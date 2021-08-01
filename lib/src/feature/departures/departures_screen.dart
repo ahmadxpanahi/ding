127e9efacee946702a5b6ec31857f7051f60fbc5
Hossein Nakhaeipoor <gnakhaeipoor@gmail.com>
2021-08-01 16:26:16 +0430

    [WIP] fix rebuild issues in departures screen loading

--- lib/src/feature/departures/departures_screen.dart
+++ lib/src/feature/departures/departures_screen.dart
@@ -43,12 +43,12 @@ class _DeparturesContainerState extends State<DeparturesContainer> {
   @override
   void initState() {
     super.initState();
+
     _controller = PageController();
     _departuresBloc = BlocProvider.of<DeparturesBloc>(context);
 
-    Future.delayed(Duration(milliseconds: 300), () {
+    Future.delayed(Duration.zero, () {
       setState(() {
-        _controller = PageController();
         _pageView = PageView(
           controller: _controller,
           onPageChanged: (i) {
@@ -62,136 +62,120 @@ class _DeparturesContainerState extends State<DeparturesContainer> {
     });
   }
 
-  Widget _buildBody() => BlocBuilder<DeparturesBloc, DeparturesState>(
+  Widget _buildTabs() => Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    _controller!.animateToPage(0,
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.linear);
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 8.2.rh,
+                    color: value == 0 ? DingColors.dark() : DingColors.light(),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/images/device.svg',
+                          width: 3.67.rh,
+                          height: 3.67.rh,
+                          color:
+                              value == 0 ? DingColors.primary() : Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 3.0.rw,
+                        ),
+                        Text(
+                          'دستگاه',
+                          style: TextStyle(
+                              fontSize: 2.73.rt,
+                              color: value == 0
+                                  ? DingColors.primary()
+                                  : Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    _controller!.animateToPage(1,
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.linear);
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 8.2.rh,
+                    color: value == 1 ? DingColors.dark() : DingColors.light(),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/images/location.svg',
+                          width: 3.67.rh,
+                          height: 3.67.rh,
+                          color:
+                              value == 1 ? DingColors.primary() : Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 3.0.rw,
+                        ),
+                        Text(
+                          'لوکیشن',
+                          style: TextStyle(
+                              fontSize: 2.73.rt,
+                              color: value == 1
+                                  ? DingColors.primary()
+                                  : Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          if (_pageView != null) Expanded(child: _pageView!),
+        ],
+      );
+
+  Widget _buildLoadingLayer() => BlocBuilder<DeparturesBloc, DeparturesState>(
         bloc: _departuresBloc,
         buildWhen: (o, n) => !(n is DoDepartureError),
         builder: (_, state) {
           if (state is DeparturesStatusState) {
-            if (state.isEnter) {
-              return EnterDeparturesScreen(progress: state.progress);
-            } else {
-              return ExitDeparturesScreen(progress: state.progress);
-            }
-          } else {
-            return Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          _controller!.animateToPage(0,
-                              duration: Duration(milliseconds: 300),
-                              curve: Curves.linear);
-                        },
-                        child: Container(
-                          alignment: Alignment.center,
-                          height: 8.2.rh,
-                          color: value == 0
-                              ? DingColors.dark()
-                              : DingColors.light(),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(
-                                'assets/images/device.svg',
-                                width: 3.67.rh,
-                                height: 3.67.rh,
-                                color: value == 0
-                                    ? DingColors.primary()
-                                    : Colors.grey,
-                              ),
-                              SizedBox(
-                                width: 3.0.rw,
-                              ),
-                              Text(
-                                'دستگاه',
-                                style: TextStyle(
-                                    fontSize: 2.73.rt,
-                                    color: value == 0
-                                        ? DingColors.primary()
-                                        : Colors.grey),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          _controller!.animateToPage(1,
-                              duration: Duration(milliseconds: 300),
-                              curve: Curves.linear);
-                        },
-                        child: Container(
-                          alignment: Alignment.center,
-                          height: 8.2.rh,
-                          color: value == 1
-                              ? DingColors.dark()
-                              : DingColors.light(),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(
-                                'assets/images/location.svg',
-                                width: 3.67.rh,
-                                height: 3.67.rh,
-                                color: value == 1
-                                    ? DingColors.primary()
-                                    : Colors.grey,
-                              ),
-                              SizedBox(
-                                width: 3.0.rw,
-                              ),
-                              Text(
-                                'لوکیشن',
-                                style: TextStyle(
-                                    fontSize: 2.73.rt,
-                                    color: value == 1
-                                        ? DingColors.primary()
-                                        : Colors.grey),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                if (_pageView != null) Expanded(child: _pageView!),
-              ],
-            );
+            return state.isEnter
+                ? EnterDeparturesScreen(progress: state.progress)
+                : ExitDeparturesScreen(progress: state.progress);
           }
+
+          return SizedBox();
         },
       );
 
+  Widget _buildBody() => Stack(
+        children: [
+          _buildTabs(),
+          _buildLoadingLayer(),
+        ],
+      );
+
   @override
-  Widget build(BuildContext context) => BlocListener(
+  Widget build(BuildContext context) => BlocListener<DeparturesBloc, DeparturesState>(
         child: _buildBody(),
         bloc: _departuresBloc,
         listener: (_, state) {
           if (state is DeparturesStatusState) {
-            if (_controller != null && !_controller!.hasClients) {
-              setState(() {
-                _controller = PageController();
-                _pageView = PageView(
-                  controller: _controller,
-                  onPageChanged: (i) {
-                    setState(() {
-                      value = i;
-                    });
-                  },
-                  children: [DevicePage(), LocationPage()],
-                );
-                Future.delayed(Duration(milliseconds: 1000), () {
-                  if (_controller?.hasClients ?? false) {
-                    _controller?.animateToPage(value,
-                        duration: Duration(milliseconds: 1500),
-                        curve: Curves.linear);
-                  }
-                });
-              });
+            if (_controller?.hasClients ?? false) {
+              _controller?.animateToPage(value,
+                  duration: Duration(milliseconds: 1500), curve: Curves.linear);
             }
           }
         },
