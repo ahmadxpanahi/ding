05607cf08a122d548ef310a42c1b293aa7ab4872
ahmad panahi <ahmadpanahi.300@gmail.com>
2021-08-05 10:38:52 +0430

    Finish map obtions

--- lib/src/feature/departures/widgets/location_page.dart
+++ lib/src/feature/departures/widgets/location_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:jalali_calendar/jalali_calendar.dart';
+import 'package:location/location.dart';
 import 'package:persian_number_utility/persian_number_utility.dart';
 import 'package:timer_builder/timer_builder.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -15,14 +16,16 @@ import 'package:latlong2/latlong.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({Key? key}) : super(key: key);
-
   @override
   _LocationPageState createState() => _LocationPageState();
 }
 
 class _LocationPageState extends State<LocationPage> {
   late DeparturesBloc _bloc;
-
+  MapController? _mapController = MapController();
+  var currentLocation = Location().getLocation();
+  LatLng? _centerLocation;
+  LatLng _myLocation = LatLng(32.852971, 59.242443);
   String timeNow =
       '${PersianDate().hour.toString().toPersianDigit()}:${PersianDate().minute.toString().toPersianDigit()}';
 
@@ -35,80 +38,98 @@ class _LocationPageState extends State<LocationPage> {
             Positioned(
               bottom: 10,
               left: 10,
-              child: Container(
-                width: 19.5.rw,
-                height: 16.0.rh,
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: TimerBuilder.periodic(Duration(seconds: 5),
-                          builder: (_) {
-                        return Text(
-                          '${PersianDate().hour.toString().toPersianDigit()}:${PersianDate().minute.toString().toPersianDigit()}',
-                          style: TextStyle(
-                              fontSize: 5.0.rw, fontWeight: FontWeight.w300),
-                        );
-                      }),
-                    ),
-                    Text(
-                      '${PersianDate().weekdayname}',
-                      style: TextStyle(fontSize: 5.0.rw - 7),
-                    ),
-                    Text(
-                      '${PersianDate().day.toString().toPersianDigit()} ${PersianDate().monthname}',
-                      style: TextStyle(fontSize: 5.0.rw - 7),
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '${'33'.toPersianDigit()}°C',
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 19.5.rw,
+                  height: 16.0.rh,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: TimerBuilder.periodic(Duration(seconds: 5),
+                            builder: (_) {
+                          return Text(
+                            '${PersianDate().hour.toString().toPersianDigit()}:${PersianDate().minute.toString().toPersianDigit()}',
                             style: TextStyle(
-                                fontSize: 5.0.rw - 5,
-                                fontWeight: FontWeight.w300),
-                          ),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          SvgPicture.asset(
-                            'assets/images/cloud.svg',
-                            width: 6.0.rw,
-                          ),
-                        ],
+                                fontSize: 5.0.rw, fontWeight: FontWeight.w300),
+                          );
+                        }),
                       ),
-                    )
-                  ],
+                      Text(
+                        '${PersianDate().weekdayname}',
+                        style: TextStyle(fontSize: 5.0.rw - 7),
+                      ),
+                      Text(
+                        '${PersianDate().day.toString().toPersianDigit()} ${PersianDate().monthname}',
+                        style: TextStyle(fontSize: 5.0.rw - 7),
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '${'33'.toPersianDigit()}°C',
+                              style: TextStyle(
+                                  fontSize: 5.0.rw - 5,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            SvgPicture.asset(
+                              'assets/images/cloud.svg',
+                              width: 6.0.rw,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            )
+            ),
+            Positioned(
+              bottom: 10,
+              right: 15,
+              child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      print(currentLocation);
+                      currentLocation.then((value) {
+                        double? long = value.longitude;
+                        double? lat = value.latitude;
+                        _myLocation = LatLng(lat!, long!);
+                      });
+                    });
+                  },
+                  child: SvgPicture.asset(
+                    'assets/images/mylocation.svg',
+                    width: 7.5.rw,
+                  )),
+            ),
+            Align(
+                alignment: Alignment.center,
+                child: SvgPicture.asset(
+                  'assets/images/pin.svg',
+                  width: 8.0.rw,
+                ))
           ],
         ),
       );
 
   Widget _map() => FlutterMap(
+        mapController: _mapController,
         options: MapOptions(
-          center: LatLng(32.852949, 59.242625),
-          zoom: 13.0,
-        ),
+            center: _myLocation,
+            zoom: 13.0,
+            onPositionChanged: (position, _) {
+              _centerLocation = position.center;
+            }),
         layers: [
-          new TileLayerOptions(
+          TileLayerOptions(
               urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
               subdomains: ['a', 'b', 'c']),
-          new MarkerLayerOptions(
-            markers: [
-              new Marker(
-                  width: 80.0,
-                  height: 80.0,
-                  point: new LatLng(32.852949, 59.242625),
-                  builder: (ctx) => Transform.scale(
-                      scale: 0.5,
-                      child: SvgPicture.asset(
-                        'assets/images/loc.svg',
-                      ))),
-            ],
-          ),
         ],
       );
 
@@ -116,6 +137,9 @@ class _LocationPageState extends State<LocationPage> {
   void initState() {
     super.initState();
     _bloc = BlocProvider.of<DeparturesBloc>(context);
+    Future.delayed(Duration(milliseconds: 1500), () {
+      _centerLocation = _mapController?.center;
+    });
   }
 
   @override
